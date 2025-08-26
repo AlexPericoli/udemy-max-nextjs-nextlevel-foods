522f96970e2d48b65b0ebff2af75dcc1b15e0a87
AlexPericoli <alexpericoli@proton.me>
2025-08-26 17:24:30 -0300

    inclui shareMeals page

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1 style={{ fontSize: 37 }}>Meal not Found</h1>
+      <p>The page or meal data you are looking for does not exist.</p>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1 style={{ fontSize: 37 }}>Page not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </main>
+  );
+}
